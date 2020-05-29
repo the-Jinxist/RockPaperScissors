d101691968a0b5d133c86aa356f46bc5304e4b59
the-Jinxist <nerosilva522@gmail.com>
2020-05-29 20:08:40 +0100

    Added image processing and interpreter code. Pending tests

--- app/src/main/java/com/exceptos/rockspaperscissors/MainActivity.kt
+++ app/src/main/java/com/exceptos/rockspaperscissors/MainActivity.kt
@@ -1,18 +1,149 @@
 package com.exceptos.rockspaperscissors
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import org.tensorflow.lite.DataType
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.common.ops.CastOp
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.BufferedReader
+import java.io.FileNotFoundException
+import java.io.FileReader
+import java.io.IOException
+import java.nio.MappedByteBuffer
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onClick(p0: View?) {
+        if (p0!! == takePictureButton){
+            val cameraIntent =  Intent(
+                Intent.ACTION_PICK
+            )
+            cameraIntent.type = "image/*"
+            if (cameraIntent.resolveActivity(packageManager) != null){
+                startActivityForResult(cameraIntent, PICK_USER_PROFILE_IMAGE)
+            }
+        }else if(p0 == takePictureButton){
+            if (imageBitmap != null){
+                val processedImage = processImage(imageBitmap!!)
+                detectHandSign((processedImage))
+            }
+        }
+    }
+
+    private lateinit var outputs: TensorBuffer
+    private lateinit var modelBuffer: MappedByteBuffer
+    private var imageBitmap: Bitmap? = null
+
+    private val PICK_USER_PROFILE_IMAGE = 10
+
+    private lateinit var detectHandSignButton: Button
+    private lateinit var takePictureButton: Button
+    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val outputs = TensorBuffer.createFixedSize(IntArray(16), DataType.FLOAT32)
+        outputs = TensorBuffer.createFixedSize(IntArray(16), DataType.FLOAT32)
+        modelBuffer = FileUtil.loadMappedFile(this, "model.tflite")
 
+        detectHandSignButton = findViewById(R.id.detect_hand_sign)
+        detectHandSignButton.isEnabled = false
+        takePictureButton = findViewById(R.id.take_picture)
+        imageView = findViewById(R.id.image_view)
+
+        takePictureButton.setOnClickListener(this)
+        detectHandSignButton.setOnClickListener(this)
 
     }
+
+    fun processImage(bitmap: Bitmap
+    ): TensorImage{
+
+        val tensorImage = TensorImage(DataType.FLOAT32)
+        tensorImage.load(bitmap)
+
+        val imageProcessor = ImageProcessor.Builder()
+            .add(ResizeOp(300, 300, ResizeOp.ResizeMethod.BILINEAR))
+            .add(ResizeWithCropOrPadOp(300, 300))
+            .add(NormalizeOp(255f, 255f))
+            .add(CastOp(DataType.FLOAT32))
+            .build()
+
+        return imageProcessor.process(tensorImage)
+    }
+
+    fun detectHandSign(image: TensorImage): IntArray{
+        val interpreter = Interpreter(modelBuffer)
+        interpreter.run(image.tensorBuffer.buffer, outputs.buffer)
+        Log.e(this.javaClass.simpleName, "Output: ${outputs.intArray}")
+        Toast.makeText(this, "Output: ${outputs.intArray}", Toast.LENGTH_LONG)
+            .show()
+        return outputs.intArray
+    }
+
+    fun convertUriToBitmap(imageUri: Uri): Bitmap{
+        var bitmap: Bitmap? = null
+        try {
+            val inputStream = contentResolver.openInputStream(imageUri)
+            bitmap = BitmapFactory.decodeStream(inputStream)
+
+            try {
+                inputStream?.close()
+            }catch (e: IOException){
+                Log.e(this.javaClass.simpleName, "Bitmap Error: ${e.message!!}")
+            }
+
+        }catch (e: FileNotFoundException){
+            Log.e(this.javaClass.simpleName, "Bitmap Error: ${e.message!!}")
+        }
+        return bitmap!!
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK){
+            if (requestCode == PICK_USER_PROFILE_IMAGE){
+                if (data != null && data.data !=  null){
+                    detectHandSignButton.isEnabled = true
+
+                    imageView.setImageURI(data.data!!)
+                    imageBitmap = convertUriToBitmap(data.data!!)
+                }
+
+            }
+        }
+    }
+
+    fun loadCSVFiles(){
+        val tsvFile = BufferedReader(FileReader("vecs.tsv"))
+        var dataRow = tsvFile.readLine()
+
+        val outputString = ""
+        while (dataRow != null){
+            outputString.plus(outputString)
+            dataRow = tsvFile.readLine()
+        }
+
+    }
+
 }
