dc7d42e540de1263c21f4ba3e189cc674c0877c5
the-Jinxist <nerosilva522@gmail.com>
2020-05-29 19:09:13 +0100

    Added Assets

--- app/src/main/java/com/exceptos/rockspaperscissors/MainActivity.kt
+++ app/src/main/java/com/exceptos/rockspaperscissors/MainActivity.kt
@@ -2,11 +2,17 @@ package com.exceptos.rockspaperscissors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val outputs = TensorBuffer.createFixedSize(IntArray(16), DataType.FLOAT32)
+
+
     }
 }
