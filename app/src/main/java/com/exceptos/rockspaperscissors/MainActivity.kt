36b71836b507219c2c7969b9a5c6efe144e35cf5
the-Jinxist <nerosilva522@gmail.com>
2020-05-30 13:33:39 +0100

    Interpreter works. Testing euclidean formular

--- app/src/main/java/com/exceptos/rockspaperscissors/MainActivity.kt
+++ app/src/main/java/com/exceptos/rockspaperscissors/MainActivity.kt
@@ -24,6 +24,7 @@ import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.io.*
 import java.nio.MappedByteBuffer
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -36,10 +37,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             if (cameraIntent.resolveActivity(packageManager) != null){
                 startActivityForResult(cameraIntent, PICK_USER_PROFILE_IMAGE)
             }
-        }else if(p0 == takePictureButton){
+        }else if(p0 == detectHandSignButton){
             if (imageBitmap != null){
+                Log.e(this.javaClass.simpleName, "Detection started")
+
                 val processedImage = processImage(imageBitmap!!)
-                detectHandSign((processedImage))
+                val outputArray = detectHandSign((processedImage))
+
+                if (mVectorOutputString.isNotEmpty() && mLabelOutputString.isNotEmpty()){
+                    performEuclideanDistanceCalculation(
+                            vectorOutputString = mVectorOutputString,
+                            labelOutputString = mLabelOutputString,
+                            detectionOutputArray = outputArray
+                    )
+                }
+
             }
         }
     }
@@ -47,6 +59,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var outputs: TensorBuffer
     private lateinit var modelBuffer: MappedByteBuffer
     private var imageBitmap: Bitmap? = null
+    private lateinit var mVectorOutputString: MutableList<String>
+    private lateinit var mLabelOutputString: MutableList<String>
 
     private val PICK_USER_PROFILE_IMAGE = 10
 
@@ -66,10 +80,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         takePictureButton = findViewById(R.id.take_picture)
         imageView = findViewById(R.id.image_view)
 
+        mVectorOutputString = mutableListOf()
+        mLabelOutputString = mutableListOf()
+
         takePictureButton.setOnClickListener(this)
         detectHandSignButton.setOnClickListener(this)
 
-        loadCSVFiles()
+        loadTSVFiles()
 
     }
 
@@ -86,16 +103,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             .add(CastOp(DataType.FLOAT32))
             .build()
 
+        Log.e(this.javaClass.simpleName, "Processing Image..")
+
         return imageProcessor.process(tensorImage)
     }
 
-    private fun detectHandSign(image: TensorImage): IntArray{
+    private fun detectHandSign(image: TensorImage): FloatArray{
+
+//        float[][] result = new float[1][labelList.size()];
+
         val interpreter = Interpreter(modelBuffer)
-        interpreter.run(image.tensorBuffer.buffer, outputs.buffer)
-        Log.e(this.javaClass.simpleName, "Output: ${outputs.intArray}")
-        Toast.makeText(this, "Output: ${outputs.intArray}", Toast.LENGTH_LONG)
+        val result = arrayOf(FloatArray(16))
+        interpreter.run(image.buffer, result)
+        Log.e(this.javaClass.simpleName, "Output: ${result[0][6]}")
+        Toast.makeText(this, "Output: ${result[0][3]}", Toast.LENGTH_LONG)
             .show()
-        return outputs.intArray
+        return result[0]
     }
 
     private fun convertUriToBitmap(imageUri: Uri): Bitmap{
@@ -133,39 +156,116 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    fun performEuclideanDistanceCalculation(vectorOutputString: MutableList<String>, labelOutputString: MutableList<String>){
+    private fun findSmallestDistance(floatList: List<Float>): Float{
+        //Finding the smallest value in the array
+        var smallestFloat = floatList[0]
+        for (float in floatList){
+            if (float < smallestFloat) smallestFloat = float
+        }
+        return smallestFloat
+    }
 
+    private fun calculateEuclideanDistance(detectionOutputArray: FloatArray, vectorArray: FloatArray): Float{
+        //The formula: sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
+        var sum = 0f
+        if (detectionOutputArray.size == vectorArray.size){
+            for(i in (0..detectionOutputArray.size - 1)){
+                sum += (detectionOutputArray[i] - vectorArray[i]) * (detectionOutputArray[i] - vectorArray[i])
+            }
+        }else{
+            Log.e(this.javaClass.simpleName, "Euclidean Calculation: The lengths of output vectors and test vectors differ")
+
+        }
+
+        return sqrt(sum)
+    }
+
+
+    /**
+     *
+     * @param vectorOutputString Contains lines of vectors seperated by tabs which will be used to test the output
+     * @param labelOutputString Contains each label by index
+     * @param detectionOutputArray The ouput we which to identify
+     */
+    fun performEuclideanDistanceCalculation(
+        vectorOutputString: MutableList<String>,
+        labelOutputString: MutableList<String>,
+        detectionOutputArray: FloatArray
+        ){
+
+        //First we initialize an empty list of floats which will contain all the
+        //euclidean distances. The index of each euclidean distance will correspond to their respective
+        //index in the list of labels we retrieved earlier
         val listOfEuclideanDistances = mutableListOf<Float>()
 
         if (vectorOutputString.size == labelOutputString.size){
             for(vectorString in vectorOutputString){
-                vectorString.split('\t').map { it.toFloat() }
+                //Taking each string, separating it be tabs and calculating the euclidean distance
+                val vectorArray = vectorString.split('\t').map { it.toFloat() }.toFloatArray()
+                Log.e(this.javaClass.simpleName, "Vector Array: $vectorArray")
+                val distance = calculateEuclideanDistance(detectionOutputArray, vectorArray)
+                //Storing this euclidean distance in a list
+                listOfEuclideanDistances.add(distance)
             }
+
+            //Finding the minimum euclidean distance
+            val smallestFloat = findSmallestDistance(listOfEuclideanDistances)
+
+            /**
+            // Corresponding it to the index of the list of labels [labelOutputString]
+            */
+            val index = listOfEuclideanDistances.indexOf(smallestFloat)
+            val label = labelOutputString[index]
+
+            notifyHandSign(label)
+
+
         }else{
-            Log.e(this.javaClass.simpleName, "Euclidean Calculation: The lengths differ")
+            Log.e(this.javaClass.simpleName, "Euclidean Calculation: The lengths of labels and test vectors differ")
         }
     }
 
-    private fun loadCSVFiles(){
+    private fun notifyHandSign(label: String) {
+        when(label){
+            "0" -> {
+                Toast.makeText(this, "This hand sign is Rock", Toast.LENGTH_LONG)
+                    .show()
+            }
+            "1" -> {
+                Toast.makeText(this, "This hand sign is Paper", Toast.LENGTH_LONG)
+                    .show()
+            }
+            "2" -> {
+                Toast.makeText(this, "This hand sign is Scissors", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+
+    private fun loadTSVFiles(){
+
+        //Loading the vector TSV files from the assets. The vectors by which we test our
+        //output
 
         val vectorsTsvFile = BufferedReader(InputStreamReader(assets.open("vecs.tsv")))
         var vectorDataRow = vectorsTsvFile.readLine()
 
-        val vectorOutputString = mutableListOf<String>()
         while (vectorDataRow != null){
-            vectorOutputString.add(vectorDataRow)
-            Log.e(this.javaClass.simpleName, "Test With: $vectorOutputString")
+            mVectorOutputString.add(vectorDataRow)
             vectorDataRow = vectorsTsvFile.readLine()
+
         }
         vectorsTsvFile.close()
+
+        //Loading the vector TSV files from the assets. The vectors by which we determine the
+        // category of our output
 
         val labelsTsvFile = BufferedReader(InputStreamReader(assets.open("labels.tsv")))
         var labelsDataRow = labelsTsvFile.readLine()
 
-        val labelsOutputString = mutableListOf<String>()
+
         while (labelsDataRow != null){
-            labelsOutputString.add(labelsDataRow)
-            Log.e(this.javaClass.simpleName, "Test With: $labelsOutputString")
+            mLabelOutputString.add(labelsDataRow)
             labelsDataRow = labelsTsvFile.readLine()
         }
         labelsTsvFile.close()
